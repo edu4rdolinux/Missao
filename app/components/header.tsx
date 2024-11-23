d682117510b817ddd60f73804b7f747cedea7eb1
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-11-23 20:49:31 -0300

    Title

---
+++ app/components/header.tsx
@@ -0,0 +1,11 @@
+import Title from "./title"
+
+const Header = () => {
+    return (
+        <div className="bg-fuchsia-400 h-16">
+            <Title/>
+        </div>
+    )
+}
+
+export default Header
---
+++ app/components/title.tsx
@@ -0,0 +1,5 @@
+const Title = () => {
+    return <h1 className="text-slate-100 m-4 text-2xl absolute">OurServices</h1>
+}
+
+export default Title
