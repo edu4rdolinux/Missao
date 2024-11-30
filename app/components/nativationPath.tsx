3a197836b728afe44a06305caceec36af49cb4f4
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-11-30 10:16:01 -0300

    Text Hover Effect

--- app/components/nativationPath.tsx
+++ app/components/nativationPath.tsx
@@ -1,10 +1,16 @@
 interface Props {
-    Text: string;
-  }
-  
-  const NavigationPath = ({ Text }: Props) => {
-    return <h1 className="text-white-primary">{Text}</h1>;
-  };
-  
-  export default NavigationPath;
-  
+  Text: string;
+}
+
+const NavigationPath = ({ Text }: Props) => {
+  return (
+    <h1 className="text-white relative group">
+      {Text}
+      <span
+        className="absolute left-0 bottom-0 w-full h-[4px] bg-white rounded-md transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
+      />
+    </h1>
+  );
+};
+
+export default NavigationPath;
