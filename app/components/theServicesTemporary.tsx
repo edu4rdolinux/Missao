29c8b26e264a408cfe2ee3dc2f2588ae2d12894c
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-02 14:03:46 -0300

    Temporary Services

---
+++ app/components/theServicesTemporary.tsx
@@ -0,0 +1,16 @@
+const TheServicesTemporary = () => {
+    return (
+        <ul>
+            <li>Вы</li>
+            <li>настолько</li>
+            <li>ленивы</li>
+            <li>что</li>
+            <li>перевели</li>
+            <li>все</li>
+            <li>это</li>
+            <li>просто</li>
+            <li>из</li>
+            <li>любопытства?</li>
+        </ul>
+    )
+}
