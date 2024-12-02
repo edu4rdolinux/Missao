3e36809685e9e2d64c348cf4bb5be0aeef9ebacc
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-02 14:00:03 -0300

    Temporarily disabling TheServices

--- app/components/ourServices.tsx
+++ app/components/ourServices.tsx
@@ -1,13 +1,15 @@
 import OurServicesTitle from "./ourServicesTitle"
-import TheServices from "./theServices"
+// import TheServices from "./theServices"
 
 const OurServices = () => {
     return (
         <div style={{ marginTop: '40em' }} className="ml-32 mt-32">
             <OurServicesTitle/>
-            <TheServices/>
+            
         </div>
     )
 }
 
 export default OurServices
+
+// <TheServices/>
--- app/page.tsx
+++ app/page.tsx
@@ -11,7 +11,7 @@ export default function Home() {
       <Body/>
       <WtButton/>
       <OurStory/>
-      <OurServices/>
+      <OurServices/> 
     </>
   );
 }
