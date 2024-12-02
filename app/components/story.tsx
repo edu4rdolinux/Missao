368955faadd4f8d9b1a03dca3198b6aba5930551
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-02 12:16:48 -0300

    Font-Family NUnito Sans

--- app/components/story.tsx
+++ app/components/story.tsx
@@ -1,5 +1,12 @@
 const Story = () => {
-    return <h1 className="text-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi<br/> nulla harum, quo modi deleniti, accusamus doloribus minima expedita<br/> iure explicabo soluta accusantium<br/> mollitia debitis esse repudiandae sit rem voluptates amet?</h1>
-}
+    return (
+        <h1 style={{ fontFamily: 'Nunito Sans, sans-serif' }} className="text-2xl font-serif">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi<br/> 
+            nulla harum, quo modi deleniti, accusamus doloribus minima expedita<br/> 
+            iure explicabo soluta accusantium<br/> 
+            mollitia debitis esse repudiandae sit rem voluptates amet?
+        </h1>
+    );
+};
 
-export default Story
+export default Story;
