822304a77dc2ab35d23cf0497f033085e4c43784
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-02 12:12:19 -0300

    Remove Bold

--- app/components/story.tsx
+++ app/components/story.tsx
@@ -1,5 +1,5 @@
 const Story = () => {
-    return <h1 className="font-bold text-4xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi<br/> nulla harum, quo modi deleniti, accusamus doloribus minima expedita iure explicabo soluta accusantium<br/> mollitia debitis esse repudiandae sit rem voluptates amet?</h1>
+    return <h1 className="text-2xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Animi<br/> nulla harum, quo modi deleniti, accusamus doloribus minima expedita<br/> iure explicabo soluta accusantium<br/> mollitia debitis esse repudiandae sit rem voluptates amet?</h1>
 }
 
 export default Story
