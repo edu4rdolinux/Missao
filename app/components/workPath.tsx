7bfac8444ea2188ebbb988aac61ffeadff76d5a7
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-03 19:03:38 -0300

    Redirect

--- app/components/workPath.tsx
+++ app/components/workPath.tsx
@@ -1,6 +1,19 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 const WorkPath = () => {
+  const router = useRouter();
+
+  const handleRedirect = () => {
+    router.push('/work');
+  };
+
   return (
-    <h1 className="text-white relative group cursor-pointer">
+    <h1
+      onClick={handleRedirect}
+      className="text-white relative group cursor-pointer"
+    >
       <span
         className="absolute left-0 bottom-0 w-full h-[4px] bg-white rounded-md transform scale-x-0 group-hover:scale-x-100 transition-transform duration-300 origin-left"
       />
