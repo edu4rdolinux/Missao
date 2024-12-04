73d062af4bf0216e90a54d8b5d3fda8372dc2a5c
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-04 16:02:00 -0300

    Scroll Reveal on Effect Text

--- app/components/effectText.tsx
+++ app/components/effectText.tsx
@@ -1,10 +1,29 @@
+"use client"
+
+import { useEffect } from 'react';
+import ScrollReveal from 'scrollreveal';
+
 const EffectWord = () => {
-    return (
-        <h1 className="m-32 text-6xl font-bold">
-            <span className="text-pink-primary">Transform</span> those small sparks<br/>
-            of possibility into the<br/> flames of a concrete <br/><span className="text-pink-primary">achievement.</span>
-        </h1>
-    );
-}
+  useEffect(() => {
+    const sr = ScrollReveal({
+      distance: '50px',
+      duration: 1000,
+      delay: 200,
+      easing: 'ease-out',
+    });
+
+    sr.reveal('.reveal', {
+      origin: 'bottom',
+      interval: 100,
+    });
+  }, []);
+
+  return (
+    <h1 className="m-32 text-6xl font-bold reveal">
+      <span className="text-pink-primary">Transform</span> those small sparks<br/>
+      of possibility into the<br/> flames of a concrete <br/><span className="text-pink-primary reveal">achievement.</span>
+    </h1>
+  );
+};
 
 export default EffectWord;
