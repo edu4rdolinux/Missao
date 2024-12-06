c1bd0a0e32a02999d6ff5b25d53584b08a35a12e
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-06 19:12:36 -0300

    Remove Services Card

--- app/components/contactUs.tsx
+++ app/components/contactUs.tsx
@@ -1,9 +1,27 @@
+"use client"
+
 import ContactUsText from "./contactUsText"
 import ContactUsTitle from "./contactUsTitle"
+import { useEffect } from "react"
+import ScrollReveal from "scrollreveal"
 
 const ContactUs = () => {
+    useEffect(() => {
+        const sr = ScrollReveal({
+          distance: '50px',
+          duration: 1000,
+          delay: 200,
+          easing: 'ease-out',
+        })
+    
+        sr.reveal('.reveal', {
+          origin: 'bottom',
+          interval: 100,
+        })
+      }, [])
+
     return (
-        <div style={{ marginTop: '40em' }} className="ml-32 mt-32">
+        <div style={{ marginTop: '40em' }} className="ml-32 mt-32 reveal">
             <ContactUsTitle/>
             <ContactUsText/>
         </div>
--- app/components/ourServices.tsx
+++ app/components/ourServices.tsx
@@ -1,9 +1,28 @@
+"use client"
+
 import OurServicesTitle from "./ourServicesTitle"
 import TheServices from "./theServices"
+import { useEffect } from "react"
+import ScrollReveal from "scrollreveal"
 
 const OurServices = () => {
+
+    useEffect(() => {
+        const sr = ScrollReveal({
+          distance: '50px',
+          duration: 1000,
+          delay: 200,
+          easing: 'ease-out',
+        })
+    
+        sr.reveal('.reveal', {
+          origin: 'bottom',
+          interval: 100,
+        })
+      }, [])
+
     return (
-        <div style={{ marginTop: '40em' }} className="ml-32 mt-32">
+        <div style={{ marginTop: '40em' }} className="ml-32 mt-32 reveal">
             <OurServicesTitle/>
             <TheServices/>
         </div>
