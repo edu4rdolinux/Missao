00dd3d3c043f695d69ad132479840337d6c46e6e
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-10 19:52:31 -0300

    Qualities

--- components/qualities.vue
+++ components/qualities.vue
@@ -1,12 +1,20 @@
 <template>
     <div class="Qualities">
-        <Card title="Mais de 200 Horas de conteúdo"/>
+        <Card title="200 Hours of Content"/>
+        <Card title="Active Support For 24/7"/>
+        <Card title="Certificate by Module"/>
+        <Card title="Connected Community"/>
+        <Card title="Over 30 Complete Games"/>
+        <Card title="Marketing for Games"/>
+        <Card title="Lifetime Access Guaranteed"/>
+        <Card title="Continuous Full Updates"/>
     </div>
 </template>
 
 <style>
 .Qualities {
     display: flex;
+    gap: 2em;
     margin-top: 1em;
     justify-content: center;
     flex-wrap: wrap;
