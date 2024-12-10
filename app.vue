fb7015514801f78c185b7af8f71771dc79f738ff
edu4rdolinux <eduardosbarros.ed@gmail.com>
2024-12-10 17:34:11 -0300

    Title

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
 <template>
   <div>
-    
+
   </div>
 </template>
+
+<style lang="scss">
+
+</style>
+
+<script setup lang="ts">
+useHead({
+  title: 'Game Dev',
+  meta: [
+    { name: 'description', content: 'Course of Game Dev' }
+  ],
+})
+</script>
